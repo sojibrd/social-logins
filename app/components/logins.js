1e476dd9945ead58e20f1c45c083905483372837
Sojib RD <43570842+sojibrd@users.noreply.github.com>
2026-02-16 23:13:18 +0600

    Add social login component with Google and Facebook integration
    - Implemented LoginsComponent for handling Google and Facebook logins.
    - Added buttons for Google and Facebook login in logins.hbs.
    - Included methods for loading Google and Facebook SDKs and handling login success.
    - Updated application template to use the new Logins component.
    - Added tests for the Logins component rendering.
    - Updated package.json and package-lock.json to include ember-simple-auth dependency.

---
+++ app/components/logins.js
@@ -0,0 +1,94 @@
+import Component from '@glimmer/component';
+import { action } from '@ember/object';
+
+export default class LoginsComponent extends Component {
+  @action
+  async loginWithGoogle() {
+    await this.loadGoogleScript();
+
+    /* global google */
+    const client = google.accounts.oauth2.initTokenClient({
+      client_id:
+        '904413682544-3jdle2rob93j7kc78ar5dl91vm03ibgq.apps.googleusercontent.com',
+      scope: 'https://www.googleapis.com/auth/userinfo.profile',
+      callback: (tokenResponse) => {
+        if (tokenResponse.access_token) {
+          this.onGoogleLoginSuccess(tokenResponse.access_token);
+        }
+      },
+    });
+
+    client.requestAccessToken();
+  }
+
+  loadGoogleScript() {
+    return new Promise((resolve) => {
+      if (typeof google !== 'undefined') {
+        return resolve();
+      }
+      const script = document.createElement('script');
+      script.src = 'https://accounts.google.com/gsi/client';
+      script.async = true;
+      script.defer = true;
+      script.onload = resolve;
+      document.body.appendChild(script);
+    });
+  }
+
+  async onGoogleLoginSuccess(accessToken) {
+    const response = await fetch(
+      'https://www.googleapis.com/oauth2/v3/userinfo',
+      {
+        headers: {
+          Authorization: `Bearer ${accessToken}`,
+        },
+      }
+    );
+    const user = await response.json();
+    console.log('User:', user);
+    alert(`Welcome, ${user.name}!`);
+  }
+
+  @action
+  async loginWithFacebook() {
+    await this.loadFacebookScript();
+
+    /* global FB */
+    FB.init({
+      appId: '1430232628491377',
+      cookie: true,
+      xfbml: true,
+      version: 'v16.0',
+    });
+
+    FB.login(
+      (response) => {
+        if (response.authResponse) {
+          this.onFacebookLoginSuccess(response.authResponse.accessToken);
+        }
+      },
+      { scope: 'public_profile,email' }
+    );
+  }
+
+  loadFacebookScript() {
+    return new Promise((resolve) => {
+      if (typeof FB !== 'undefined') {
+        return resolve();
+      }
+      const script = document.createElement('script');
+      script.src = 'https://connect.facebook.net/en_US/sdk.js';
+      script.async = true;
+      script.defer = true;
+      script.onload = resolve;
+      document.body.appendChild(script);
+    });
+  }
+
+  onFacebookLoginSuccess(accessToken) {
+    FB.api('/me', { fields: 'name,email' }, (response) => {
+      console.log('User:', response);
+      alert(`Welcome, ${response.name}!`);
+    });
+  }
+}
